efd2d823f77dec624003ede9f96ad9a43b1d96b5
PedoneMatteo <m.pedone2000@gmail.com>
2026-01-01 19:18:12 +0000

    start

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read Video
-    video_frames = read_video('input_videos/match.mp4')
+    video_frames = read_video('input_videos/match.mp4')[:300] # Process only first n frames for demo
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
